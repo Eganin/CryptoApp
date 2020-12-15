d8d929cab5ef04b888fc2d26e9259f35ca81a5cf
Eganin <egorzaharin12@gmail.com>
2020-12-15 21:16:47 +0500

    add dao coin price info

--- app/src/main/java/com/example/cryptoapp/database/AppDatabase.kt
+++ app/src/main/java/com/example/cryptoapp/database/AppDatabase.kt
@@ -1,21 +1,34 @@
 package com.example.cryptoapp.database
 
 import android.content.Context
+import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.cryptoapp.data.dao.CoinPriceInfoDao
+import com.example.cryptoapp.data.pojo.CoinPriceInfo
 
+@Database(entities = [CoinPriceInfo::class],version = 1,exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun coinPriceInfoDao(): CoinPriceInfoDao
+
     companion object {
         private var db: AppDatabase? = null
         private const val NAME_DB = "crypto.db"
 
         fun getInstance(context: Context): AppDatabase {
             synchronized(AppDatabase::class.java) {
-                if (db == null) {
-                    db = Room.databaseBuilder(context, AppDatabase::class.java, NAME_DB).build()
-                }
-
+                db?.let { return it }
+                val instance =
+                    Room.databaseBuilder(
+                        context,
+                        AppDatabase::class.java,
+                        NAME_DB
+                    ).build()
+                db = instance
+                return instance
             }
         }
+
     }
 }
