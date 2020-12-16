9a758e1d4a1815a284b808f6f107f5aaea268925
Eganin <egorzaharin12@gmail.com>
2020-12-16 23:35:33 +0500

    refactoring viewmodel

--- app/src/main/java/com/example/cryptoapp/fragments/main/CoinListFragment.kt
+++ app/src/main/java/com/example/cryptoapp/fragments/main/CoinListFragment.kt
@@ -1,10 +1,12 @@
 package com.example.cryptoapp.fragments.main
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.cryptoapp.R
 import com.example.cryptoapp.application.CryptoApp
@@ -22,6 +24,11 @@ class CoinListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         coinViewModel = ViewModelProviders.of(this@CoinListFragment)[CoinViewModel::class.java]
-        coinViewModel.loadData(api =(activity?.application as CryptoApp).apiService)
+        //coinViewModel.priceList.observe(this@CoinListFragment , Observer{
+        //    println(it.toString())
+        //})
+        coinViewModel.getDetailInfo(fromSymbol = "BTC").observe(this, Observer{
+           Log.d("AAA",it.toString())
+        })
     }
 }
--- app/src/main/java/com/example/cryptoapp/fragments/main/CoinViewModel.kt
+++ app/src/main/java/com/example/cryptoapp/fragments/main/CoinViewModel.kt
@@ -1,63 +1,76 @@
 package com.example.cryptoapp.fragments.main
 
 import android.app.Application
-import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import com.example.cryptoapp.api.ApiService
+import com.example.cryptoapp.application.CryptoApp
 import com.example.cryptoapp.data.pojo.CoinPriceInfo
 import com.example.cryptoapp.data.pojo.CoinPriceInfoRawData
 import com.example.cryptoapp.database.AppDatabase
 import com.google.gson.Gson
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
+import java.util.concurrent.TimeUnit
 
 
 class CoinViewModel(application: Application) : AndroidViewModel(application) {
     private val database = AppDatabase.getInstance(context = application)
-    private var coinsList = database.coinPriceInfoDao()
+    var priceList = database.coinPriceInfoDao().getPriceList()
     private val compositeDisposable = CompositeDisposable()
     val errors = MutableLiveData<Throwable>()
+
+    init {
+        loadData(api = (application as CryptoApp).apiService)
+    }
 
     override fun onCleared() {
         super.onCleared()
         compositeDisposable.dispose()
     }
 
-    fun loadData(api: ApiService?) {
-        api?.let {
-            compositeDisposable.add(
-                it.getTopCoinInto()
-                    //с первой загрузкой применяем map
-                    .map { it.data?.map { it.coinInfo?.name }?.joinToString(",") }
-                    // применение действия над каждым значением map
-                    .flatMap { api.getFullPriceList(fromSymbol = it) }
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe({
-                        Log.d("CoinViewModel", it.toString())
-                    }, {
-                        errors.value = it
-                    })
-            )
-        }
+    fun getDetailInfo(fromSymbol: String) =
+        database.coinPriceInfoDao().getPriceInfoAboutCoin(fsym = fromSymbol)
+
+    private fun loadData(api: ApiService) {
+        compositeDisposable.add(
+            api.getTopCoinInto()
+                //с первой загрузкой применяем map
+                .map { it.data?.map { it.coinInfo?.name }?.joinToString(",") }
+                // применение действия над каждым значением map
+                .flatMap { api.getFullPriceList(fromSymbol = it) }
+                .map { getPriceListFromRawData(coinPriceInfoRawData = it) }
+                // бесконечно повторяем загрузку
+                .repeat()
+                // выполнить загрузку заново если предыдущая упадет
+                .retry()
+                // repeat повторяется через какое-то время
+                .delaySubscription(10, TimeUnit.SECONDS)
+                .subscribeOn(Schedulers.io())
+                //.observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    database.coinPriceInfoDao().insertPriceList(list = it!!)
+                }, {
+                    //errors.value = it
+                })
+        )
     }
 
     private fun getPriceListFromRawData(
         coinPriceInfoRawData: CoinPriceInfoRawData
     ): List<CoinPriceInfo> {
         val result = ArrayList<CoinPriceInfo>()
-        val json = coinPriceInfoRawData.coinPriceInfoJsonData
-        if (json == null) return result
-        val coinKeySet = json.keySet()
-        for (i in coinKeySet) {
-            val currency = json.getAsJsonObject(i)
-            val currencyKeySet = currency.keySet()
+        val jsonObject = coinPriceInfoRawData.coinPriceInfoJsonData ?: return result
+        val coinKeySet = jsonObject.keySet()
+        for (coinKey in coinKeySet) {
+            val currencyJson = jsonObject.getAsJsonObject(coinKey)
+            val currencyKeySet = currencyJson.keySet()
             for (currencyKey in currencyKeySet) {
-                val priceInfo =
-                    Gson().fromJson(json.getAsJsonObject(currencyKey), CoinPriceInfo::class.java)
-                result.add(element = priceInfo)
+                val priceInfo = Gson().fromJson(
+                    currencyJson.getAsJsonObject(currencyKey),
+                    CoinPriceInfo::class.java
+                )
+                result.add(priceInfo)
             }
         }
         return result
